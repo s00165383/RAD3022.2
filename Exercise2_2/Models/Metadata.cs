bafd2d883b083ffed5d9d72c446f935f580b074c
s00165383 <s00165383@mail.itsligo.ie>
2018-01-25 10:49:33 +0000

    partial complete

---
+++ Exercise2_2/Models/Metadata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Exercise2_2.Models
+{
+    public class CustomerMetadata
+    {
+        public int Id;
+
+        [Required(ErrorMessage = "First Name is Required")]
+        [StringLength(25, MinimumLength = 2)]
+        public string FirstName;
+
+        [Required(ErrorMessage = "Last Name is Required")]
+        [StringLength(25, MinimumLength = 2)]
+        public string LastName;
+
+        [Required(ErrorMessage = "Email Address is Required")]
+        [EmailAddress]
+        public string Email;
+    }
+
+    [MetadataType(typeof(CustomerMetadata))]
+    public partial class Customer{}
+}
